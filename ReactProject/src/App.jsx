cc4d0e8dba3a1906f4e3862b03cd31dbdc4a94c9
DurgaDevi <durgadevir0906@gmail.com>
2025-06-18 16:13:07 +0530

    Day2 Commit

---
+++ ReactProject/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <h1>Hello, Oliver Wade</h1>
+    </div>
+    <div>
+      <h1>Have a good one</h1>
+    </div>
+    
+    </>
+   
+
+  )
+}
+
+export default App
