c2f94daab5b67503b36d0f149db422fd8b6593a0
DurgaDevi <durgadevir0906@gmail.com>
2025-06-20 16:06:49 +0530

    DAY4

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Child from './Componenet/Child.jsx'
 import Home from './Pages/Home.jsx'
 import About from './Pages/About.jsx'
@@ -6,23 +7,40 @@ import Skills from './Pages/Skills.jsx'
 import Contact from './Pages/Contact.jsx'
 import Login from './Pages/Login.jsx'
 import Counter from './Pages/Counter.jsx'
-
+import Navbar from './Componenet/Navbar.jsx'
+import State from './Hooks/State.jsx'
+import Hooks from './Pages/Hooks.jsx'
+import Effect from './Hooks/Effect.jsx'
 
 const App = () => {
 
-  var array = ['Oliver' ,'Wade','Max','Hadrien','Stein'];
-  var login = {username:'durgadevi',password:'durga0906'}
+  var array = ['Oliver', 'Wade', 'Max', 'Hadrien', 'Stein'];
+  var login = { username: 'durgadevi', password: 'durga0906' }
   return (
     <div>
-      <Child name="Oliver" dept="FrontEnd Developer" contact="8807671271"/>
+      {/* <Child name="Oliver" dept="FrontEnd Developer" contact="8807671271"/>
       <Home items={array} user={login}/>
       <About/>
       <Skills/>
       <Contact/>
       <Login userlogin={login}/>
-      <Counter/>
+      <Counter/> */}
+      <Navbar />
+      <Routes>
+        <Route path='/home' element={<Home items={array} user={login} />} />
+        <Route path='/about' element={<About />}/>
+        <Route path='/counter' element={<Counter />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/skills' element={<Skills />}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/hooks' element={<Hooks />}/>
+        <Route path='/state' element={<State />}/>
+        <Route path='/effect' element={<Effect />}/>
+      </Routes>
+
     </div>
-    
+
   )
 }
 
--- ReactProject/src/Pages/Login.jsx
+++ ReactProject/src/Pages/Login.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Login.css'
 const Login = () => {
+  const[formData,setFormData]=useState({
+    email:'',
+    password:''
+  })
+  const handleChange=(e)=>{
+    setFormData((prev)=>({...prev,[e.target.name]:e.target.value}))
+  }
+  const handelSubmit=(e)=>{
+    e.preventDefault();
+    console.log(formData);
+    setFormData({
+      email:'',
+      password:''
+    })
+  }
   return (
-    <div>
-      <h1>Employee Form</h1>
-      <form action="">
-        <label htmlFor="name">Name:</label>
-        <input type="text" /><br /><br />
-        <label htmlFor="dept">Department:</label>
-        <input type="text" /><br /><br />
-        <label htmlFor="email">Email ID:</label>
-        <input type="email" /><br /><br />
-        <label htmlFor="phno">PhoneNo:</label>
-        <input type="text" /><br /><br />
-        <button type="submit" onClick={()=>alert("Form Submitted Successfully")}>Submit</button>
+    <div className='main'>
+
+      <form action="" onSubmit={handelSubmit}>
+              <h1 className='heading'>LOGIN</h1>
+        <table>
+          <tr>
+            <th><label>EMAIL:</label></th>
+            <td><input type="email" name='email' value={formData.email} onChange={handleChange}/></td>
+          </tr>
+          <tr>
+            <th><label>PASSWORD:</label></th>
+            <td><input type="password" name='password' value={formData.password} onChange={handleChange}/></td>
+          </tr>
+          <tr>
+            <th><button>LOGIN</button></th>
+          </tr>
+        </table>
+        
+        
+        
+        
       </form>
     </div>
   )
